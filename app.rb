393110722ffe3669f30f2a38d359ec59395b8103
Julian Higman <jhigman@pobox.com>
2013-09-11 22:31:02 +0100

    Use env vars instead of config file for google creds.

--- app.rb
+++ app.rb
@@ -15,16 +15,14 @@ class GDocTestRunner < Sinatra::Application
   enable :sessions
   set :session_secret, 'my sooper secret'
 
-  config = YAML.load_file(File.join('config', 'config.yml'))
-
-  set :username, config['username']
-  set :password, config['password']
+  set :username, ENV["GOOGLE_DRIVE_USERNAME"]
+  set :password, ENV["GOOGLE_DRIVE_PASSWORD"]
 
   if ENV["REDISCLOUD_URL"]
     uri = URI.parse(ENV["REDISCLOUD_URL"])
     set :redis, Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   else
-    set :redis, Redis.new(:host => config['redis_host'], :port => config['redis_port'], :db => config['redis_db'])
+    set :redis, Redis.new
   end
   
 end
