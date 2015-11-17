f2808e42ae5e7f56904ee473e0e054f47000dcb7
Julian Higman <jhigman@pobox.com>
2015-11-17 15:35:20 +0000

    Allow jobs to be cloned and queued from summary page

---
+++ Rakefile
@@ -0,0 +1,3 @@
+require './app'
+require 'resque/tasks'
+
--- routes/job.rb
+++ routes/job.rb
@@ -7,22 +7,58 @@ end
 
 get '/jobs' do
   @jobs = settings.store.load_jobs
+  haml :jobs
+end
+
+get '/jobs/summary' do
+  @jobs = settings.store.load_jobs
   if request["host"]
-    @jobs.select!{|job| job.host == request["host"]}
+    @host = request["host"]
+    @jobs.select!{|job| job.host == @host}
   end
   if request["spreadsheet"]
+    @spreadsheet = request["spreadsheet"]
     @jobs.select!{|job| job.spreadsheet == request["spreadsheet"]}
   end
-  if request["summary"]
-    summary_jobs = []
-    @jobs.each do |job|
-      if !summary_jobs.any?{|selected_job| selected_job.worksheet == job.worksheet}
-        summary_jobs << job
+  summary_jobs = []
+  @jobs.each do |job|
+    if !summary_jobs.any?{|selected_job| selected_job.worksheet == job.worksheet}
+      summary_jobs << job
+    end
+  end
+  @jobs = summary_jobs
+  haml :summary
+end
+
+post '/jobs/summary' do
+
+  params = []
+  if !request["host"].empty?
+    @host = request["host"]
+    params << "host=#{@host}"
+  end
+  if !request["spreadsheet"].empty?
+    @spreadsheet = request["spreadsheet"]
+    params << "spreadsheet=#{@spreadsheet}"
+  end
+
+  if request[:job_ids]
+    request[:job_ids].keys.each do |job_id|
+      job = settings.store.load_job(job_id)
+      if job
+        new_job = Sprat::Job.new
+        new_job.spreadsheet = job.spreadsheet
+        new_job.worksheet = job.worksheet
+        new_job.host = job.host
+        new_job.local = job.local
+        new_job.created_at = Time.now
+        new_job = settings.store.save_job(new_job)
+        Resque.enqueue(Sprat::Job, new_job.id)
       end
     end
-    @jobs = summary_jobs
   end
-  haml :jobs
+
+  redirect "/jobs/summary?" + params.join('&')
 end
 
 get '/jobs/:id' do
